a27cc41b84e59a39c0e49f1aa6c3b13f50cf41cc
Dan Croak <dcroak@thoughtbot.com>
2009-07-01 16:44:46 -0400

    adding user model test by default. fixing SMTP settings.

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,5 +1,3 @@
-config.action_mailer.delivery_method = :smtp
-
 ActionMailer::Base.smtp_settings = {
   :tls            => true,
   :address        => "smtp.gmail.com",
---
+++ test/unit/user_test.rb
@@ -0,0 +1,9 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  should "be valid with factories" do
+    assert_valid Factory.build(:user)
+    assert_valid Factory.build(:email_confirmed_user)
+  end
+end
+

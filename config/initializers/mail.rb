3ee9c831db04485c602e496cd9b06e44c7c5e187
Dan Croak <dcroak@thoughtbot.com>
2009-07-02 03:15:15 -0400

    mailer should use config vars

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,11 +1,16 @@
+# Use config vars to get your Gmail credentials onto Heroku.
+# They will be automatically picked up by this file in production.
+
+# heroku config:add GMAIL_EMAIL=dcroak@example.com GMAIL_PASSWORD=password
+
 require 'smtp-tls'
 
 ActionMailer::Base.smtp_settings = {
   :address        => "smtp.gmail.com",
   :port           => "587",
   :authentication => :plain,
-  :user_name      => "YOUREMAIL",
-  :password       => "YOURPASSWORD"
+  :user_name      => ENV['GMAIL_EMAIL'],
+  :password       => ENV['GMAIL_PASSWORD']
 }
 
 DO_NOT_REPLY = "donotreply@example.com"

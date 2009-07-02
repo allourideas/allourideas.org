a1cb219055390ae872e2d47ef38767a13a452532
Dan Croak <dcroak@thoughtbot.com>
2009-07-01 23:34:35 -0400

    setting correct gmail configuration

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,8 +1,8 @@
+require 'smtp-tls'
+
 ActionMailer::Base.smtp_settings = {
-  :tls            => true,
   :address        => "smtp.gmail.com",
   :port           => "587",
-  :domain         => "heroku.com",
   :authentication => :plain,
   :user_name      => "YOUREMAIL",
   :password       => "YOURPASSWORD"

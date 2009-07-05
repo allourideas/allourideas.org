55c00dcb33a393a2cf4c48c466cb831d5167d20c
Dan Croak <dcroak@thoughtbot.com>
2009-07-05 17:38:10 -0400

    app crashed during boot on heroku without rubaidh require in google analytics initializer

--- config/initializers/google_analytics.rb
+++ config/initializers/google_analytics.rb
@@ -1,1 +1,5 @@
-Rubaidh::GoogleAnalytics.tracker_id = ENV['GOOGLE_ANALYTICS_TRACKER_ID'] if ENV['GOOGLE_ANALYTICS_TRACKER_ID']
+require 'rubaidh/google_analytics'
+
+if ENV['GOOGLE_ANALYTICS_TRACKER_ID']
+  Rubaidh::GoogleAnalytics.tracker_id = ENV['GOOGLE_ANALYTICS_TRACKER_ID']
+end

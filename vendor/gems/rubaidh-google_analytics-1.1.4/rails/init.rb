177b3289eb14e584dfcaefbac0153cc981f82187
Dan Croak <dcroak@thoughtbot.com>
2009-07-31 14:17:48 -0400

    removing google analytics vendored gem. keeping it in production env, .gems for heroku

--- vendor/gems/rubaidh-google_analytics-1.1.4/rails/init.rb
+++
@@ -1,5 +0,0 @@
-require 'rubaidh/google_analytics'
-require 'rubaidh/view_helpers'
-ActionController::Base.send :include, Rubaidh::GoogleAnalyticsMixin
-ActionController::Base.send :after_filter, :add_google_analytics_code
-ActionView::Base.send :include, Rubaidh::GoogleAnalyticsViewHelper
--- vendor/gems/rubaidh-google_analytics-1.1.4/test/test_helper.rb
+++
@@ -1,8 +0,0 @@
-ENV['RAILS_ENV'] = 'test'
-
-require 'rubygems'
-require 'test/unit'
-
-require File.expand_path(File.dirname(__FILE__) + '/../lib/rubaidh/google_analytics.rb')
-require File.expand_path(File.dirname(__FILE__) + '/../lib/rubaidh/view_helpers.rb')
-

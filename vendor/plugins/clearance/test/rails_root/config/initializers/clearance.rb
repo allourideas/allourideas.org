8a3201bc4322e1ef9190ad3f2dedca14daeff934
Dan Croak <dcroak@thoughtbot.com>
2009-08-13 22:29:36 -0400

    removing clearance plugin. adding clearance 0.7.0 gem.

--- vendor/plugins/clearance/test/rails_root/config/initializers/clearance.rb
+++
@@ -1,8 +0,0 @@
-# This simulates loading the clearance gem, but without relying on
-# vendor/gems
-
-clearance_path = File.join(File.dirname(__FILE__), *%w(.. .. .. ..))
-clearance_lib_path = File.join(clearance_path, "lib")
-
-$LOAD_PATH.unshift(clearance_lib_path)
-load File.join(clearance_path, 'rails', 'init.rb')

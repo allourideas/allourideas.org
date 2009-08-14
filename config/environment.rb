21eb6ccf2af55e864598342685c2d0e33178dbd4
Dan Croak <dcroak@thoughtbot.com>
2009-08-13 22:49:40 -0400

    making sure .gems and config/environment.rb are in sync, complete

--- config/environment.rb
+++ config/environment.rb
@@ -21,12 +21,19 @@ Rails::Initializer.run do |config|
     :lib     => "delayed_job",
     :version => "1.7.0",
     :source  => "http://gems.github.com"
-  config.gem "thoughtbot-paperclip",
+  config.gem "tristandunn-paperclip",
     :lib     => "paperclip",
-    :version => "2.2.9.2",
+    :version => "2.3.1",
     :source  => "http://gems.github.com"
-  config.gem "right_aws",
-    :version => "1.10.0"
+  config.gem "mime-types",
+    :lib     => "mime/types",
+    :version => "1.16"
+  config.gem "xml-simple",
+    :lib     => "xmlsimple",
+    :version => "1.0.12"
+  config.gem "aws-s3",
+    :lib     => "aws/s3",
+    :version => "0.6.2"
   config.gem "yfactorial-utility_scopes",
     :lib     => "utility_scopes",
     :version => "0.2.2",
@@ -35,5 +42,13 @@ Rails::Initializer.run do |config|
     :lib     => 'formtastic', 
     :source  => 'http://gems.github.com',
     :version => '0.2.1'
+  config.gem "josevalim-inherited_resources", 
+    :lib     => 'inherited_resources', 
+    :source  => 'http://gems.github.com',
+    :version => '0.8.5'
+  config.gem "thoughtbot-clearance", 
+    :lib     => 'clearance', 
+    :source  => 'http://gems.github.com', 
+    :version => '0.7.0'
 end
 

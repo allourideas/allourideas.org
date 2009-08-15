04a83107e14efcbfe85f1a7d46b9e4c2efe33c44
Dan Croak <dcroak@thoughtbot.com>
2009-08-15 07:19:09 -0400

    fix for paperclip webrat step when model is two words

--- features/step_definitions/paperclip_steps.rb
+++ features/step_definitions/paperclip_steps.rb
@@ -1,6 +1,6 @@
 When /^I attach an? "([^\"]*)" file to an? "([^\"]*)" on S3$/ do |filename, klass|
   filename   = filename.to_sym
-  definition = klass.classify.constantize.attachment_definitions[filename]
+  definition = klass.gsub(" ", "_").classify.constantize.attachment_definitions[filename]
 
   path = "http://s3.amazonaws.com/:id/#{definition[:path]}"
   path.gsub!(/:([^\/\.]+)/) do |match|

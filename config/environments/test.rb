80ae556b56b5c19a3dfd928c631f6e3dbc88ea8d
Dan Croak <dcroak@thoughtbot.com>
2009-08-14 23:30:47 -0400

    load mocha last

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -23,10 +23,6 @@ config.action_mailer.delivery_method = :test
 
 # rake gems:install RAILS_ENV=test
 
-config.gem 'jferris-mocha',
-  :version => '0.9.5.0.1241126838',
-  :source  => 'http://gems.github.com',
-  :lib     => 'mocha'
 config.gem 'thoughtbot-factory_girl', 
   :lib     => 'factory_girl',
   :source  => 'http://gems.github.com',
@@ -41,6 +37,10 @@ config.gem 'jtrupiano-timecop',
   :version => '0.2.1'
 config.gem 'fakeweb',
   :version => '>= 1.2.5'
+config.gem 'jferris-mocha',
+  :version => '0.9.5.0.1241126838',
+  :source  => 'http://gems.github.com',
+  :lib     => 'mocha'
 
 HOST = 'localhost'
 

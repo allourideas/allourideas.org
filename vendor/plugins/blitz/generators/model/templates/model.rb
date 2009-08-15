1c682aaf11cfbb4e8d5098435ea7b2eaab1f3aba
Dan Croak <dcroak@thoughtbot.com>
2009-08-15 07:26:43 -0400

    blitz is not a gem

--- vendor/plugins/blitz/generators/model/templates/model.rb
+++ vendor/plugins/blitz/generators/model/templates/model.rb
@@ -3,6 +3,13 @@ class <%= class_name %> < ActiveRecord::Base
   belongs_to :<%= each.name %>
 <% end -%>
 <% attributes.select { |each| each.type == :paperclip }.each do |each| -%>
-  has_attached_file :<%= each.name %>
+  has_attached_file :<%= each.name %>,
+   :path           => ":attachment/:id/:style.:extension",
+   :storage        => :s3,
+   :s3_credentials => {
+     :access_key_id     => ENV['S3_KEY'],
+     :secret_access_key => ENV['S3_SECRET']
+   },
+   :bucket         => ENV['S3_BUCKET']
 <% end -%>
 end

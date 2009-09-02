f4625fab84570867b05d5555eee1673d57ee646d
Daniel Croak <dancroak@dan-lawlesss-macbook.local>
2009-09-02 17:51:41 -0400

    upgrading treetop

--- config/environments/cucumber.rb
+++ config/environments/cucumber.rb
@@ -22,7 +22,7 @@ config.gem 'polyglot',
   :version => '0.2.8',
   :lib     => false
 config.gem 'treetop',
-  :version => '1.2.6',
+  :version => '1.3.0',
   :lib     => false
 config.gem 'term-ansicolor',
   :version => '1.0.3',

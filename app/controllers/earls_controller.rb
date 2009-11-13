9f2292c66b73c07678bdd545858f22489e8badbb
Pius Uzamere <pius@alum.mit.edu>
2009-11-12 23:50:22 -0500

    friendly

--- app/controllers/earls_controller.rb
+++ app/controllers/earls_controller.rb
@@ -1,6 +1,6 @@
 class EarlsController < ApplicationController
   def show
-    @earl = Earl.find(:first, :conditions => {:name => params[:id]})
+    @earl = Earl.find(params[:id])
     #redirect_to(@earl ? @earl.question : '/' ) and return
     if @earl
       @question = @earl.question#the question has a prompt id with it

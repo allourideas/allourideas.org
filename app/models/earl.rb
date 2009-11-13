5ea6574be89b07b5af6da050a9225dad02e2c701
Pius Uzamere <pius@alum.mit.edu>
2009-11-12 23:36:20 -0500

    earls are back

--- app/models/earl.rb
+++ app/models/earl.rb
@@ -1,9 +1,9 @@
 class Earl < ActiveRecord::Base
-  # validates_presence_of :question_id, :on => :create, :message => "can't be blank"
-  # validates_presence_of :name, :on => :create, :message => "can't be blank"
-  # #has_friendly_id :name, :use_slug => true
-  # 
-  # def question
-  #   Question.find(question_id)
-  # end
+  validates_presence_of :question_id, :on => :create, :message => "can't be blank"
+  validates_presence_of :name, :on => :create, :message => "can't be blank"
+  #has_friendly_id :name, :use_slug => true
+  
+  def question
+    Question.find(question_id)
+  end
 end

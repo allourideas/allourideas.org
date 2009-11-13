f21dd73abdf03185fdd13c93e66356315f34cf69
Pius Uzamere <pius@alum.mit.edu>
2009-11-13 11:22:10 -0500

    colors

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,6 +5,42 @@
 
 jQuery(document).ready(function() {
 	
+	[$("#logo-citp"), $("#logo-princeton"), $("#logo-open"), $("#logo-check")]).each(function(el) {
+    $(this).bind("mouseover", function() {
+      var str = $(this).attr("src");
+      $(this).attr('src', str.replace(".jpg", "-down.jpg"));
+    });
+    $(this).bind("mouseout", function() {
+      var str = $(this).attr("src");
+      $(this).attr('src', str.replace("-down.jpg", ".jpg"));
+    });
+  });
+  $(".votebox tr.prompt td.idea").each(function(el) {
+    $(this).bind("click", function() {
+      $([$(this).children(".round-filledfg"), $(this).children(".round-filled").children()]).each(function(el) {
+        $(this).css("background", "#0B0");
+        $(this).css("border-left", "1px solid #0B0");
+        $(this).css("border-right", "1px solid #0B0");
+      });
+      $(this).unbind("mouseover")
+      $(this).unbind("mouseout")
+    });
+    $(this).bind("mouseover", function() {
+      $([$(this).children(".round-filledfg"), $(this).children(".round-filled").children()]).each(function(el) {
+        $(this).css("background", "#2b88ad");
+        $(this).css("border-left", "1px solid #2b88ad");
+        $(this).css("border-right", "1px solid #2b88ad");
+      });
+    });
+    $(this).bind("mouseout", function() {
+      $([$(this).children(".round-filledfg"), $(this).children(".round-filled").children()]).each(function(el) {
+        $(this).css("background", "#3198c1");
+        $(this).css("border-left", "1px solid #3198c1");
+        $(this).css("border-right", "1px solid #3198c1");
+      });
+    });})
+  
+	
 	humanMsg.setup();
 	$("#tabs").tabs();
 	

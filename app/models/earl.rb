dc01625c862b277cb4c4e470989320f136c75c3c
Pius Uzamere <pius@alum.mit.edu>
2009-11-13 07:36:17 -0500

    reserving some names

--- app/models/earl.rb
+++ app/models/earl.rb
@@ -1,7 +1,7 @@
 class Earl < ActiveRecord::Base
   validates_presence_of :question_id, :on => :create, :message => "can't be blank"
   validates_presence_of :name, :on => :create, :message => "can't be blank"
-  has_friendly_id :name, :use_slug => true
+  has_friendly_id :name, :use_slug => true, :reserved => ["questions", "question", 'about', 'privacy', 'tour']
   
   def question
     Question.find(question_id)

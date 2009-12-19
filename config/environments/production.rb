e1e71166c2c608569a4c6f536565d7fee873cc34
Pius Uzamere <pius@uyiosa.com>
2009-12-18 21:22:43 -0500

    switching the host up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,6 @@ config.gem "rubaidh-google_analytics",
   :source  => "http://gems.github.com"
 
 HOST = 'http://allourideas.heroku.com' #"http://www.allourideas.org"
-API_HOST = ENV['PAIRWISE_API_HOST'] #e.g. "http://pairwise.heroku.com"
+API_HOST = ENV['PAIRWISE_API_HOST']
 PAIRWISE_USERNAME = ENV['PAIRWISE_USERNAME']
 PAIRWISE_PASSWORD = ENV['PAIRWISE_PASSWORD']

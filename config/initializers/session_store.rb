453a489f4ea423ba7e9a47c376cc5fb5243c3aa8
Pius Uzamere <pius@uyiosa.com>
2009-12-23 00:40:45 -0500

    session tweak

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,8 +5,8 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :session_key => "_allourideas_session",
-  :secret      => "6dba8105d5aea328b336942988bcfd80"
+  :session_key => "_allourideas_ey_session",
+  :secret      => "6dba8105d5aea328b336942988bcfd80erwe"
 }
 
 # Use the database for sessions instead of the cookie-based default,

9a67487f3339bb250852f20a5a9993a5d5c183d4
Pius Uzamere <pius@uyiosa.com>
2010-01-06 12:39:16 -0500

    google analytics, updated hoptoad

--- config/initializers/google_analytics.rb
+++ config/initializers/google_analytics.rb
@@ -1,6 +1,6 @@
 # Google Analytics is only available in production by default
 # http://github.com/rubaidh/google_analytics
 
-if ENV['GOOGLE_ANALYTICS_TRACKER_ID']
-  Rubaidh::GoogleAnalytics.tracker_id = ENV['GOOGLE_ANALYTICS_TRACKER_ID']
+if RAILS_ENV == 'production'#ENV['GOOGLE_ANALYTICS_TRACKER_ID']
+  Rubaidh::GoogleAnalytics.tracker_id = 'UA-11703548-1' #ENV['GOOGLE_ANALYTICS_TRACKER_ID']
 end
--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,5 +1,5 @@
-if ENV['HOPTOAD_API_KEY']
+#if ENV['HOPTOAD_API_KEY']
   HoptoadNotifier.configure do |config|
-    config.api_key = ENV['HOPTOAD_API_KEY']
+    config.api_key = 'a3047d4d5791fa2298cd52c42fd7231d'#ENV['HOPTOAD_API_KEY']
   end
-end
+#end

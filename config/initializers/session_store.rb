31f810688446cab4dcd92c2209491a4ee6fd135e
Pius Uzamere <pius@uyiosa.com>
2010-01-15 10:42:06 -0500

    remove the need to delete cookies when viewing in staging

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,10 +4,18 @@
 # If you change this key, all old sessions will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
+case RAILS_ENV
+when 'staging'
 ActionController::Base.session = {
-  :session_key => "_allourideas_ey_session_#{RAILS_ENV}",
+  :session_key => "_allourideas_ey_session_#{RAILS_ENV}_#{Time.now.inspect}",
   :secret      => "6dba8105d5aea328b336942988bcfd80erwe"
 }
+else
+  ActionController::Base.session = {
+    :session_key => "_allourideas_ey_session_#{RAILS_ENV}",
+    :secret      => "6dba8105d5aea328b336942988bcfd80erwe"
+  }
+end
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information

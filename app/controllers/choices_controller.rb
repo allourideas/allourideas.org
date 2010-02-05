968eccea2c72274b734ded76442e22ee3eea49b5
Dhruv Kapadia <git@dkapadia.com>
2010-02-04 22:31:17 -0500

    Fixed typo to allow for easy activate/deactivae. Added more descriptive flash messages

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -54,7 +54,11 @@ class ChoicesController < ApplicationController
   
   
   def activate
-    authenticate and return unless current_user
+    authenticate 
+    if !current_user
+    	flash[:for_real] = "You must be logged in to perform this action"
+	return
+    end
     
     @question = Question.find_by_name(params[:question_id])
     logger.info "actual question ID is #{@question.id}"
@@ -63,25 +67,43 @@ class ChoicesController < ApplicationController
     #@choice.activate! if @choice
     
     redirect_to('/') and return unless current_user == @question.creator
+    if current_user == @question.creator
     
-    @choice.put(:update_from_abroad, :params => {:question_id => @question.id}) if @choice
-    flash[:for_real] = "You have successfully activated the idea <strong>#{@choice.attributes['data']}</strong>"
-    logger.info flash[:notice]
-    # redirect_to("#{@question.earl}/choices/#{@choice.id}") and return
-    redirect_to("#{@question.earl}") and return
+    	@choice.put(:update_from_abroad, :params => {:question_id => @question.id}) if @choice
+    	flash[:for_real] = "You have successfully activated the idea <strong>#{@choice.attributes['data']}</strong>"
+    	logger.info flash[:notice]
+    	# redirect_to("#{@question.earl}/choices/#{@choice.id}") and return
+    	redirect_to("#{@question.earl}") and return
+    else
+    	flash[:for_real] = "You do not have permission to modify this question"
+	redirect_to('/sign_in') and return
+    end
   end
   
   
-  def activate
+  def deactivate
+    authenticate 
+    if !current_user
+    	flash[:for_real] = "You must be logged in to perform this action"
+	return
+    end
+
     @question = Question.find_by_name(params[:question_id])
     logger.info "actual question ID is #{@question.id}"
     @choice = Choice.find(params[:id], :params => {:question_id => @question.id})
     logger.info "Found choice: #{@choice.inspect}"
-    @choice.put(:update_from_abroad, :params => {:question_id => @question.id}) if @choice
-    flash[:for_real] = "You have successfully activated the idea <strong>#{@choice.attributes['data']}</strong>"
-    logger.info flash[:notice]
-    # redirect_to("#{@question.earl}/choices/#{@choice.id}") and return
-    redirect_to("#{@question.earl}") and return
+
+    # should probably put an error message for those not logged in 
+    if current_user == @question.creator
+    	@choice.put(:deactivate_from_abroad, :params => {:question_id => @question.id}) if @choice
+    	flash[:for_real] = "You have successfully deactivated the idea <strong>#{@choice.attributes['data']}</strong>"
+    	logger.info flash[:notice]
+    	# redirect_to("#{@question.earl}/choices/#{@choice.id}") and return
+    	redirect_to("#{@question.earl}") and return
+    else
+    	flash[:for_real] = "You do not have permission to modify this question"
+	redirect_to('/sign_in') and return
+    end
   end
   
   

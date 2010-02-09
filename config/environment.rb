9088b40e2d5d3dcddbb8c0aef610f0a216d34310
Dhruv Kapadia <git@dkapadia.com>
2010-02-09 16:55:49 -0500

    Timezone fixes and add-box width change

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
 
-  config.time_zone = 'UTC'
+  config.time_zone = 'Eastern Time (US & Canada)'
 
   config.action_mailer.delivery_method = :sendmail
   config.gem 'hoptoad_notifier'

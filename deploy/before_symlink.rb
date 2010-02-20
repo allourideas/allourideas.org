459983401b73c3a16bf0fd34f0c490a50d996903
Dhruv Kapadia <git@dkapadia.com>
2010-02-19 21:05:23 -0500

    Fix symlink in deploy link

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,2 +1,2 @@
 run "echo 'release_path: #{release_path}/db/GeoIPCity.dat' >> #{shared_path}/logs.log"
-run "ln -nfs #{shared_path}/shared/GeoIPCity.dat #{release_path}/db/GeoIPCity.dat"
+run "ln -nfs #{shared_path}/GeoLiteCity.dat #{release_path}/db/GeoLiteCity.dat"

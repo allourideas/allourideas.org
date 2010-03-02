2f57d94e995aead076c9039cb075ed4c95e381d9
Dhruv Kapadia <git@dkapadia.com>
2010-03-02 18:52:08 -0500

    Actually add redis-store to production. Hopefully get rid of pernicous bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@
 
 # The production environment is meant for finished, "live" apps.
 # Code is not reloaded between requests
-config.cache_classes = false
+config.cache_classes = true
 
 # Use a different logger for distributed setups
 # config.logger = SyslogLogger.new
@@ -13,6 +13,9 @@ config.action_controller.perform_caching             = true
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host                  = "http://assets.example.com"
+
+require 'redis-store'
+Abingo.cache = ActiveSupport::Cache::RedisStore.new
 
 # Disable delivery errors, bad email addresses will be ignored
 config.action_mailer.raise_delivery_errors = false

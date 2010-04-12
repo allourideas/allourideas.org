382a138bc459f57c589dec5be7cc51a6f164f762
Dhruv Kapadia <git@dkapadia.com>
2010-04-12 15:20:20 -0400

    Adding default_url_options so locale will stick between sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -139,4 +139,10 @@ class ApplicationController < ActionController::Base
 	  I18n.locale = params[:locale]
   end
 
+
+  def default_url_options(options={})
+	  logger.debug "default_url_options is passed options: #{options.inspect}\n"
+	  { :locale => I18n.locale }
+  end
+
 end

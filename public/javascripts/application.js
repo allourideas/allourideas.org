a1cf6b2fc08cd3f5ecb496e927d22084696f9788
Dhruv Kapadia <git@dkapadia.com>
2010-04-20 11:11:13 -0400

    Fixing bug in displaying indicator on votermap

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -36,7 +36,7 @@ function increment(number)
 	return parseInt(number) + 1;
 }
 function iframe_loaded(){
-	   $('#voter_map_indicator').hide();
+	   $('.voter_map_indicator').hide();
 }
 
 
@@ -533,7 +533,7 @@ jQuery(document).ready(function() {
 			var target_row = $(this).parent().parent().next();
 			if(!toggleLinkTextandTargetElement($(this), target_row))
 			{
-			        var iframe_html= "<tr id=voter_map_row class='row1'><td class='title' colspan='2' height=370px><div id='voter_map_indicator'><img src='/images/indicator.gif' /></div><iframe id='voter_map_iframe' src='" + $(this).attr('href') + "' onload='iframe_loaded();' width='746px' height='370px' frameborder=0 scrolling=no style='border:1px solid rgb(145,145,145);'></iframe></td></tr>"
+			        var iframe_html= "<tr id=voter_map_row class='row1'><td class='title' colspan='2' height=370px><div class='voter_map_indicator'><img src='/images/indicator.gif' /></div><iframe id='voter_map_iframe' src='" + $(this).attr('href') + "' onload='iframe_loaded();' width='746px' height='370px' frameborder=0 scrolling=no style='border:1px solid rgb(145,145,145);'></iframe></td></tr>"
 				current_row.after(iframe_html);
 //				$('#view_voter_map_row').after("<tr id=voter_map_row class='row1'><td class='title' height=360px colspan='2' style='text-align:center'><div id='geo_map_canvas'><img src=/images/indicator.gif /></div></td></tr>")
 //				$.get($(this).attr("href")+".js", null, null, "script");

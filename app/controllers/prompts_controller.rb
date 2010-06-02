c003055095ac0222288f72f5fedcc4db1d650735
Chap Ambrose <chapambrose@gmail.com>
2010-06-02 16:49:23 -0400

    merge extra info onto response if @photocracy?

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -1,5 +1,6 @@
-class PromptsController < ActionController::Base
+class PromptsController < ApplicationController
   include ActionView::Helpers::TextHelper
+
   def vote
     bingo!("voted")
     voted_prompt = Prompt.find(params[:id], :params => {:question_id => params[:question_id]})
@@ -17,15 +18,27 @@ class PromptsController < ActionController::Base
 
       next_prompt = Crack::XML.parse(vote.body)['prompt']
       leveling_message = Visitor.leveling_message(:votes => next_prompt['visitor_votes'].to_i,
-					          :ideas => next_prompt['visitor_ideas'].to_i)
-      logger.info(next_prompt.inspect)
-      render :json => {
-        :newleft           => truncate(next_prompt['left_choice_text'], 137),
-        :newright          => truncate(next_prompt['right_choice_text'], 137),
+					                                        :ideas => next_prompt['visitor_ideas'].to_i)
+
+      result = {
+        :newleft           => truncate(next_prompt['left_choice_text'], {:length => 137}),
+        :newright          => truncate(next_prompt['right_choice_text'], {:length => 137}),
         :appearance_lookup => next_prompt['appearance_id'],
         :prompt_id         => next_prompt['id'],
         :leveling_message  => leveling_message,
-      }.to_json
+      }
+
+      if @photocracy
+        result.merge!({
+          :visitor_votes  => next_prompt['visitor_votes'],
+          :newright_photo => Photo.find(next_prompt['right_choice_text']).image.url(:medium),
+          :newleft_photo  => Photo.find(next_prompt['left_choice_text']).image.url(:medium),
+          :newleft_url    => vote_question_prompt_url(params[:question_id], next_prompt['id'], :direction => :left),
+          :newright_url   => vote_question_prompt_url(params[:question_id], next_prompt['id'], :direction => :right)
+        })
+      end
+
+      render :json => result.to_json
     else
       render :text => 'Vote unsuccessful.', :status => :unprocessable_entity
     end

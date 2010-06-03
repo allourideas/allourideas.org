f32d33f4ae1a10f3f48813861720c2cc1311b710
Chap Ambrose <chapambrose@gmail.com>
2010-06-03 14:45:23 -0400

    Got the voting history updating

--- public/javascripts/photocracy/application.js
+++ public/javascripts/photocracy/application.js
@@ -37,19 +37,31 @@ function castVote(choice) {
 }
 
 function updateVotingHistory(data) {
+	var winner = data['voted_prompt_winner']
 	$('#visitor_votes').text(data['visitor_votes']);
-	// todo
-	// add the last prompt to the voting history
+	$('#your_votes').prepend("\
+		<li>\
+			<img src='" + $('.left').attr('thumb') + "' class='" + (winner == 'left' ? 'winner' : 'loser') + "'/>\
+			<img src='" + $('.right').attr('thumb') + "' class='" + (winner == 'right' ? 'winner' : 'loser') + "'/>\
+			<abbr class='timeago' title='" + data['voted_at'] + "'>" + data['voted_at'] + "</abbr>\
+		</li>\
+		");
+	$('#your_votes').children(":first").effect("highlight", {}, 3000);
+	$("abbr.timeago").timeago();
 }
+
 
 function loadNextPrompt(data) {
 	// remove spinner and checkmark
-	$('a.vote').removeClass('loading');
-	$('a.vote').removeClass('chosen');
+	$('a.vote').removeClass('loading chosen');
 
 	// change photos
-	$('a.vote.left').css('background', "url('" + data['newleft_photo'] + "') center center no-repeat");
-	$('a.vote.right').css('background', "url('" + data['newright_photo'] + "') center center no-repeat");
+	$('a.vote.left').css('background', "#fff url('" + data['newleft_photo'] + "') center center no-repeat");
+	$('a.vote.right').css('background', "#fff url('" + data['newright_photo'] + "') center center no-repeat");
+	
+	// change photo thumbs
+	$('a.vote.left').attr('thumb', data['newleft_photo_thumb']);
+	$('a.vote.right').attr('thumb', data['newright_photo_thumb']);
 
 	// change href urls
 	$('a.vote.left').attr('href', data['newleft_url']);

d749a36e8591e1da3c89549e616d14a0b3c1da09
Chap Ambrose <chapambrose@gmail.com>
2010-06-14 13:56:17 -0400

    don't forget the redis command

--- aaa_remember.rb
+++ aaa_remember.rb
@@ -1,7 +1,10 @@
-Steps to prepare testing 
+# Steps to prepare testing 
 Pairwise:
   rake testserver:launch
 AOI: 
   script/console cucumber
     Abingo.cache.clear
   rake db:test:prepare
+  
+# start redis
+rake redis:start

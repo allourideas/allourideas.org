35282ddd1a1622d6a6ec9d5de22463acfb9755a0
Chap Ambrose <chapambrose@gmail.com>
2010-06-22 13:00:25 -0400

    Organized princeton and philly seed images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,24 @@
-@user = User.create(:email => 'chapambrose@gmail.com', 
-                    :password => 'password', 
-                    :password_confirmation => 'password')
-                   
-@question = Question.create(:name => 'Which photo is more Philly?',
-                            :url => 'philly',
+@user = User.find_or_create_by_email(:email => 'chapambrose@gmail.com',
+                                     :password => 'password',
+                                     :password_confirmation => 'password')
+
+photo_ids = (1..3).to_a
+@question = Question.create(:name => 'Which photo is more Princeton?',
+                            :url => 'princeton',
                             :local_identifier => @user.id,
                             :visitory_identifier => 123,
-                            :ideas => "1 \n 2 \n 3")
+                            :ideas => photo_ids.join("\n"))
 
 Earl.create(:question_id => @question.id,
             :name => @question.url,
-            :user_id => @user.id)
+            :user_id => @user.id,
+            :flag_enabled => true)
                    
                    
 require 'action_controller'
 require 'action_controller/test_process.rb'
-(1..3).to_a.each do |i|
-  Photo.create! :image => ActionController::TestUploadedFile.new("#{Rails.root}/db/seed-images/#{i}.jpg", "image/jpeg")
+photo_ids.each do |i|
+  Photo.create! :image => ActionController::TestUploadedFile.new("#{Rails.root}/db/seed-images/princeton/#{i}.jpg", "image/jpg")
   # Added these in the question.create above
   # Choice.post(:create_from_abroad, :question_id => @question.id, :params => {'auto' => rand(472348), :data => @photo.id, :local_identifier => @user.id})
 end

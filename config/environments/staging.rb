d798f6cee8399bed6088953ded7fc6748a141c47
Dhruv Kapadia <git@dkapadia.com>
2010-07-27 15:09:30 -0400

    Fixing typo

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -25,7 +25,7 @@ config.gem "rubaidh-google_analytics",
 config.action_mailer.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
  :address => "smtp.sendgrid.net",
-  :port = > '25',
+  :port => '25',
   :authentication => :plain,
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD']

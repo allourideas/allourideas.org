3ff6456a0e45f0c3068e461053d25a0faca3ff71
Dhruv Kapadia <git@dkapadia.com>
2010-08-05 14:51:05 -0400

    Remove stackoverflow ab test

--- app/controllers/earls_controller.rb
+++ app/controllers/earls_controller.rb
@@ -132,7 +132,6 @@ class EarlsController < ApplicationController
        @ab_test_name = (params[:id] == 'studentgovernment') ? "studgov_test_size_of_X_votes_on_Y_ideas2" : 
        								"#{@earl.name}_#{@earl.question_id}_test_size_of_X_votes_on_Y_ideas"	       
        @ab_test_ideas_text_name = "#{@earl.name}_#{@earl.question_id}_test_contents_of_add_idea_button"	       
-       @ab_test_stackoverflow_name = "#{@earl.name}_#{@earl.question_id}_test_stackoverflow_welcome"	       
     else
       redirect_to('/') and return
     end

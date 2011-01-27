e1e1d87890d48dce117204ea861b024f08fbea31
Luke Baker <lukebaker@gmail.com>
2011-01-27 08:53:40 -0500

    add symlink to redis.yml in deploy hook

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,2 +1,6 @@
 run "echo 'release_path: #{release_path}/db/GeoIPCity.dat' >> #{shared_path}/logs.log"
 run "ln -nfs #{shared_path}/GeoLiteCity.dat #{release_path}/db/GeoLiteCity.dat"
+
+if FileTest.exists?("#{shared_path}/config/redis.yml")
+  run "ln -nfs #{shared_path}/config/redis.yml #{release_path}/config/redis.yml"
+end

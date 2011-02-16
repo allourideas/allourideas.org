9ef16cc112f6d85b7b31d2456e9cf07bb0733351
Luke Baker <lukebaker@gmail.com>
2011-02-16 11:36:48 -0500

    change from CONCAT to CONCAT_WS so we ignore nulls

--- app/models/export.rb
+++ app/models/export.rb
@@ -4,7 +4,8 @@ class Export < ActiveRecord::Base
   # This allows us to append to the record.
   # NOTE: This may only work with MySQL
   def self.update_concat(id, data)
-    sql = "UPDATE `exports` SET `data` = CONCAT(`data`, '%s') WHERE id = '%s'"
+    # use CONCAT_WS so we ignore NULL values
+    sql = "UPDATE `exports` SET `data` = CONCAT_WS('', `data`, '%s') WHERE id = '%s'"
     ActiveRecord::Base.connection.execute sanitize_sql_array([sql, data, id])
   end
 end

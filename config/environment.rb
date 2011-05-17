b09994442a972812cdf70f3e3318a122422a32f6
Luke Baker <lukebaker@gmail.com>
2011-05-17 11:47:12 -0400

    upgrade newrelic_rpm

--- config/environment.rb
+++ config/environment.rb
@@ -75,7 +75,7 @@ Rails::Initializer.run do |config|
   config.gem 'haml',
 	  :version => '3.0.9'
   config.gem 'newrelic_rpm',
-	  :version => '2.13.4'
+	  :version => '3.0.0'
 end
 
 PRODUCTION_API_HOST = ENV['API_HOST']

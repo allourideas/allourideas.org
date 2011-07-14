7b0e49090ca3a923d00013c8a6bbd70c2590185f
Luke Baker <lukebaker@gmail.com>
2011-07-14 10:08:03 -0400

    add before_create filter to modify tracking variable to info

--- app/models/click.rb
+++ app/models/click.rb
@@ -2,10 +2,29 @@ class Click < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :session_info
 
+  before_create :translate_tracking_to_info
+
   # checks referrer to see if this click was done on a widget
   def widget?
     return false if referrer.blank?
     uri = URI.parse(referrer)
     referrer.include?('widget') || (!uri.host.nil? && uri.host.include?('iphone'))
   end
+
+  private
+    def translate_tracking_to_info
+      {:url => url, :referrer => referrer}.each do |k, value|
+        if value && value.match(/[?&]tracking=/)
+          begin
+            querystring = CGI.parse(URI.parse(value).query)
+            unless querystring.has_key?('info')
+              self[k] = value.gsub(/([?&])tracking=/, '\1info=')
+            end
+          rescue
+            nil
+          end
+        end
+      end
+    end
+
 end

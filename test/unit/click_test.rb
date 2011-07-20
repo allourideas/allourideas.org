cca0b28dbacccb1afb0d5f6144a65e6b702ee262
Luke Baker <lukebaker@gmail.com>
2011-07-20 09:48:48 -0400

    add tests for click creation and translation from tracking to info

--- test/unit/click_test.rb
+++ test/unit/click_test.rb
@@ -7,4 +7,31 @@ class ClickTest < ActiveSupport::TestCase
   should "be valid with factory" do
     assert_valid Factory.build(:click)
   end
+
+  should "convert tracking to info when first parameter" do
+    click = Click.create(
+      :url => 'http://example.com/?tracking=foo&bar=baz',
+      :referrer => 'http://example.com/?tracking=foo&bar=baz'
+    )
+    assert_equal 'http://example.com/?info=foo&bar=baz', click.url
+    assert_equal 'http://example.com/?info=foo&bar=baz', click.referrer
+  end
+
+  should "convert tracking to info when second parameter" do
+    click = Click.create(
+      :url => 'http://example.com/?bar=baz&tracking=foo',
+      :referrer => 'http://example.com/?bar=baz&tracking=foo'
+    )
+    assert_equal 'http://example.com/?bar=baz&info=foo', click.url
+    assert_equal 'http://example.com/?bar=baz&info=foo', click.referrer
+  end
+
+  should "not convert tracking to info when info parameter exists" do
+    click = Click.create(
+      :url => 'http://example.com/?info=baz&tracking=foo',
+      :referrer => 'http://example.com/?info=baz&tracking=foo'
+    )
+    assert_equal 'http://example.com/?info=baz&tracking=foo', click.url
+    assert_equal 'http://example.com/?info=baz&tracking=foo', click.referrer
+  end
 end

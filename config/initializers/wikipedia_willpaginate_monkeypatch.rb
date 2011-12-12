62490607aae84f0dae2974da1e0d64bca33ee419
Chap Ambrose <chapambrose@gmail.com>
2011-12-12 09:36:40 -0600

    add special will_paginate render for wikipedia

---
+++ config/initializers/wikipedia_willpaginate_monkeypatch.rb
@@ -0,0 +1,21 @@
+class WikipediaPagination < WillPaginate::LinkRenderer
+
+  def page_link_or_span(page, span_class, text = nil)
+    text ||= page.to_s
+    if span_class.include?('prev_page')
+      classname = 'prev' 
+    elsif span_class.include?('next_page')
+      classname = 'next'
+    else
+      classname = ''
+    end
+    classname += ' active' if page == current_page
+    span_class = span_class.gsub('disabled','') if page
+    if page && page != current_page
+      @template.content_tag :li, @template.link_to(text, url_for(page)), :class => [classname, span_class].join(' ')
+    else
+      @template.content_tag :li, @template.link_to(text, url_for(page)), :class => [classname, span_class].join(' ')
+    end
+  end
+
+end

a25b6eae3f22b837d2e966c4b5576cf3673f19b9
Luke Baker <lukebaker@gmail.com>
2012-07-13 08:58:23 -0400

    fix bug with showing close button on accordions
    it wasn't showing after initial load because we were returning early

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -21,10 +21,10 @@ AOI.app = (function($) {
     function dataVisualizationOpen() {
         $('.collapse').on('show', function(ev) {
             var target = $(ev.target);
-            if (target.data('loaded') === true) { return; }
             var group = target.closest('.accordion-group');
             var link = group.find('.accordion-heading a:first');
             group.find('button.close').show();
+            if (target.data('loaded') === true) { return; }
             if (target.is('.map')) {
                 var iframe = $("<iframe src='" + link.attr('href') + "' width='746px' height='370px' frameborder=0 scrolling=no></iframe>");
                 target.find('.accordion-inner').html(iframe);

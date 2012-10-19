eafb909b5db63e35c3873a92a141a558e638e891
Luke Baker <lukebaker@gmail.com>
2012-10-19 11:27:30 -0400

    add additional route to mock

--- spec/integration/voting_spec.rb
+++ spec/integration/voting_spec.rb
@@ -8,6 +8,7 @@ describe 'A User on the vote page' do
       prompt   = Factory.build(:prompt, :id => 1).attributes.to_xml(:root => 'prompt')
       prompt2  = Factory.build(:prompt, :id => 2).attributes.to_xml(:root => 'prompt')
       mock.get "/questions/1.xml?visitor_identifier=test123&with_appearance=true&with_prompt=true&with_visitor_stats=true", {}, question, 200
+      mock.get "/questions/1.xml?visitor_identifier=test123&with_appearance=true&with_average_votes=true&with_prompt=true&with_visitor_stats=true", {}, question, 200
       mock.get "/questions/1/prompts/1.xml", {}, prompt, 200
       mock.post "/questions/1/prompts/1/vote.xml?next_prompt%5Bvisitor_identifier%5D=test123&next_prompt%5Bwith_appearance%5D=true&next_prompt%5Bwith_visitor_stats%5D=true&question_id=1&vote%5Bappearance_lookup%5D=f72da54add43e5ca39cab80f1c72f0e7&vote%5Bdirection%5D=left&vote%5Bskip_fraud_protection%5D=true&vote%5Btime_viewed%5D=5&vote%5Btracking%5D%5Bx_click_offset%5D=&vote%5Btracking%5D%5By_click_offset%5D=&vote%5Bvisitor_identifier%5D=test123", {}, prompt2, 200
     end

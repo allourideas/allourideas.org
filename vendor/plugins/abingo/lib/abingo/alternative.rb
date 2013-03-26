70933e2cb2b45b85d08dd35251c3bf1483f53bfa
Luke Baker <lukebaker@gmail.com>
2013-03-26 15:12:06 -0400

    create trial object when user sees new experiment

--- vendor/plugins/abingo/lib/abingo/alternative.rb
+++ vendor/plugins/abingo/lib/abingo/alternative.rb
@@ -20,6 +20,11 @@ class Abingo::Alternative < ActiveRecord::Base
   def self.score_participation(test_name)
     viewed_alternative = Abingo.find_alternative_for_user(test_name,
       Abingo::Experiment.alternatives_for_test(test_name))
+
+    lookup = self.calculate_lookup(test_name, viewed_alternative)
+    chosen_alt = Abingo::Alternative.find_by_lookup(lookup)
+    Trial.create(:session_info_id => Abingo.identity, :alternative_id => chosen_alt.id)
+
     self.update_all("participants = participants + 1", :lookup => self.calculate_lookup(test_name, viewed_alternative))
   end
 

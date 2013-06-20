a2dc37c0741b64644523c65b1c66a6f3a39b6836
Luke Baker <lukebaker@gmail.com>
2013-06-20 16:37:29 -0400

    add cache backend for i18n

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,2 +1,4 @@
 I18n.backend.class.send(:include, I18n::Backend::Fallbacks)
+I18n.backend.class.send(:include, I18n::Backend::Cache)
+I18n.cache_store = ActiveSupport::Cache.lookup_store(:memory_store)
 I18n.fallbacks.map('fr' => 'en')

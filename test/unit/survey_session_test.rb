dc4f14eea7782f8c8479dc4a8879f859ae92b4e2
Luke Baker <lukebaker@gmail.com>
2014-02-17 15:11:38 -0500

    add some more tests for survey_session

--- test/unit/survey_session_test.rb
+++ test/unit/survey_session_test.rb
@@ -7,6 +7,44 @@ class SurveySessionTest < ActiveSupport::TestCase
     verifier.generate(value)
   end
 
+  context "the initialize method" do
+    should "generate a session_id if none given" do
+      s = SurveySession.new({:question_id => 1})
+      assert_not_nil(s.session_id)
+    end
+
+    should "use given cookie_name" do
+      s = SurveySession.new({:question_id => 1, :session_id => 'sessionid'})
+      assert_equal(s.session_id, 'sessionid')
+    end
+
+    should "generate a cookie_name if none given" do
+      s = SurveySession.new({:question_id => 1})
+      assert_not_nil(s.cookie_name)
+    end
+
+    should "use given cookie_name" do
+      s = SurveySession.new({:question_id => 1}, 'aoi_1_abc')
+      assert_equal(s.cookie_name, 'aoi_1_abc')
+    end
+
+    should "create the expiration time if none given" do
+      s = SurveySession.new({:question_id => 1})
+      assert(!s.expired?)
+      Timecop.travel(10.minutes.from_now) do
+        assert(s.expired?)
+      end
+    end
+
+    should "use given expiration time" do
+      s = SurveySession.new({:question_id => 1, :expiration_time => 5.minutes.from_now.utc})
+      assert(!s.expired?)
+      Timecop.travel(5.minutes.from_now.utc) do
+        assert(s.expired?)
+      end
+    end
+  end
+
   def exception_test(test)
     exception = assert_raises(CantFindSessionFromCookies, "ARGS: #{test[:args].inspect}") { SurveySession.send(:find, *test[:args]) }
     assert_equal(test[:message], exception.message, "ARGS: #{test[:args].inspect}")
@@ -65,36 +103,37 @@ class SurveySessionTest < ActiveSupport::TestCase
     should "raise an exception when the question id does not match cookie and a lookup is sent" do
       exception_test({:message => 'Question ID did not match cookie name', :args => [{'aoi_1_abc' => c({:question_id => 2, :appearance_lookup => 'lookup'})}, 1, 'lookup']})
     end
-  end
 
-  should "return the session data when found" do
-    match_session = {
-      :session => 'match',
-      :question_id => 1,
-      :appearance_lookup => 'lookup'
-    }
-    match_2_session = {
-      :session => 'match',
-      :question_id => 2,
-      :appearance_lookup => 'lookup'
-    }
-    no_match_session = {
-      :session => 'match',
-      :question_id => 1,
-      :appearance_lookup => 'no_lookup'
-    }
-    test_matrix = [
-      {:args => [{'aoi_1_abc' => c(match_session)}, 1], :result => [match_session, 'aoi_1_abc']},
-      {:args => [{'aoi_1_abc' => c(match_session)}, '1'], :result => [match_session, 'aoi_1_abc']},
-      {:args => [{'aoi_1_abc' => c(match_session), 'aoi_1_def' => c(no_match_session)}, 1], :result => [match_session, 'aoi_1_abc']},
-      {:args => [{'aoi_1_abc' => c(match_session), 'aoi_2_aaa' => c(no_match_session)}, 1], :result => [match_session, 'aoi_1_abc']},
-      {:args => [{'aoi_1_abc' => c(no_match_session), 'aoi_2_aaa' => c(match_2_session)}, 2], :result => [match_2_session, 'aoi_2_aaa']},
-      {:args => [{'aoi_1_lookup' => c(match_session), 'aoi_1_aaa' => c(no_match_session)}, 1, 'lookup'], :result => [match_session, 'aoi_1_lookup']},
-      {:args => [{'aoi_1_lookup' => c(match_session)}, 1, 'lookup'], :result => [match_session, 'aoi_1_lookup']},
-      {:args => [{'aoi_1_lookup' => c(match_session)}, '1', 'lookup'], :result => [match_session, 'aoi_1_lookup']},
-    ]
-    test_matrix.each do |test|
-      assert_equal(SurveySession.send(:find, *test[:args]), test[:result], "ARGS: #{test[:args].inspect}")
+    should "return the session data when found" do
+      match_session = {
+        :session => 'match',
+        :question_id => 1,
+        :appearance_lookup => 'lookup'
+      }
+      match_2_session = {
+        :session => 'match',
+        :question_id => 2,
+        :appearance_lookup => 'lookup'
+      }
+      no_match_session = {
+        :session => 'match',
+        :question_id => 1,
+        :appearance_lookup => 'no_lookup'
+      }
+      test_matrix = [
+        {:args => [{'aoi_1_abc' => c(match_session)}, 1], :result => [match_session, 'aoi_1_abc']},
+        {:args => [{'aoi_1_abc' => c(match_session)}, '1'], :result => [match_session, 'aoi_1_abc']},
+        {:args => [{'aoi_1_abc' => c(match_session), 'aoi_1_def' => c(no_match_session)}, 1], :result => [match_session, 'aoi_1_abc']},
+        {:args => [{'aoi_1_abc' => c(match_session), 'aoi_2_aaa' => c(no_match_session)}, 1], :result => [match_session, 'aoi_1_abc']},
+        {:args => [{'aoi_1_abc' => c(no_match_session), 'aoi_2_aaa' => c(match_2_session)}, 2], :result => [match_2_session, 'aoi_2_aaa']},
+        {:args => [{'aoi_1_lookup' => c(match_session), 'aoi_1_aaa' => c(no_match_session)}, 1, 'lookup'], :result => [match_session, 'aoi_1_lookup']},
+        {:args => [{'aoi_1_lookup' => c(match_session)}, 1, 'lookup'], :result => [match_session, 'aoi_1_lookup']},
+        {:args => [{'aoi_1_lookup' => c(match_session)}, '1', 'lookup'], :result => [match_session, 'aoi_1_lookup']},
+      ]
+      test_matrix.each do |test|
+        assert_equal(SurveySession.send(:find, *test[:args]), test[:result], "ARGS: #{test[:args].inspect}")
+      end
     end
   end
+
 end

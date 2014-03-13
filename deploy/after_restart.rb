95c070eb33272354f0bd17e0d4de932b3d52c170
Luke Baker <lukebaker@gmail.com>
2014-03-13 08:58:40 -0400

    add config. prefix to deploy script
    fix for node variable access

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,4 +1,4 @@
 #send hoptoad deploy notification
-run "cd #{config.release_path} && rake hoptoad:deploy TO=#{node[:environment][:framework_env]} USER=#{node[:owner_name]} REVISION=#{`cat #{config.release_path}/REVISION`}"
+run "cd #{config.release_path} && rake hoptoad:deploy TO=#{config.node[:environment][:framework_env]} USER=#{config.node[:owner_name]} REVISION=#{`cat #{config.release_path}/REVISION`}"
 # restart delayed job workers
 run "sudo monit restart all -g All_Our_Ideas_jobs"

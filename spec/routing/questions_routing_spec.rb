95d4d4bf028f09d36d28354a3a2a9dc0dfaf4b03
Aivils Štoss <aivils@ithouse.lv>
2018-09-20 10:05:49 +0300

    rails5 rspec questions routing

--- spec/routing/questions_routing_spec.rb
+++ spec/routing/questions_routing_spec.rb
@@ -1,63 +1,33 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'spec_helper'
 
 describe QuestionsController do
   describe "route generation" do
     it "maps #index" do
-      route_for(:controller => "questions", :action => "index").should == "/questions"
+      expect(:get => "/questions").to route_to(:controller => "questions", :action => "index")
     end
 
     it "maps #new" do
-      route_for(:controller => "questions", :action => "new").should == "/questions/new"
+      expect(:get => "/questions/new").to route_to(:controller => "questions", :action => "new")
     end
 
     it "maps #show" do
-      route_for(:controller => "questions", :action => "show", :id => "1").should == "/questions/1"
+      expect(:get => "/questions/1").to route_to(:controller => "questions", :action => "show", :id => "1")
     end
 
     it "maps #edit" do
-      route_for(:controller => "questions", :action => "edit", :id => "1").should == "/questions/1/edit"
+      expect(:get => "/questions/1/edit").to route_to(:controller => "questions", :action => "edit", :id => "1")
     end
 
     it "maps #create" do
-      route_for(:controller => "questions", :action => "create").should == {:path => "/questions", :method => :post}
+      expect(:post => "/questions").to route_to(:controller => "questions", :action => "create")
     end
 
     it "maps #update" do
-      route_for(:controller => "questions", :action => "update", :id => "1").should == {:path =>"/questions/1", :method => :put}
+      expect(:put => "/questions/1").to route_to(:controller => "questions", :action => "update", :id => "1")
     end
 
     it "maps #destroy" do
-      route_for(:controller => "questions", :action => "destroy", :id => "1").should == {:path =>"/questions/1", :method => :delete}
-    end
-  end
-
-  describe "route recognition" do
-    it "generates params for #index" do
-      params_from(:get, "/questions").should == {:controller => "questions", :action => "index"}
-    end
-
-    it "generates params for #new" do
-      params_from(:get, "/questions/new").should == {:controller => "questions", :action => "new"}
-    end
-
-    it "generates params for #create" do
-      params_from(:post, "/questions").should == {:controller => "questions", :action => "create"}
-    end
-
-    it "generates params for #show" do
-      params_from(:get, "/questions/1").should == {:controller => "questions", :action => "show", :id => "1"}
-    end
-
-    it "generates params for #edit" do
-      params_from(:get, "/questions/1/edit").should == {:controller => "questions", :action => "edit", :id => "1"}
-    end
-
-    it "generates params for #update" do
-      params_from(:put, "/questions/1").should == {:controller => "questions", :action => "update", :id => "1"}
-    end
-
-    it "generates params for #destroy" do
-      params_from(:delete, "/questions/1").should == {:controller => "questions", :action => "destroy", :id => "1"}
+      expect(:delete => "/questions/1").to route_to(:controller => "questions", :action => "destroy", :id => "1")
     end
   end
 end

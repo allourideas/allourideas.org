dfe482d2fb0391c95b4cfc9ed5076ba42e8bad3d
Aivils Štoss <aivils@ithouse.lv>
2018-09-26 17:00:35 +0300

    rails5 remove active storage

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,24 @@
 require_relative 'boot'
 
-require 'rails/all'
+# require 'rails/all'
+require "rails"
+
+# active_storage/engine
+%w(
+  active_record/railtie
+  action_controller/railtie
+  action_view/railtie
+  action_mailer/railtie
+  active_job/railtie
+  action_cable/engine
+  rails/test_unit/railtie
+  sprockets/railtie
+).each do |railtie|
+  begin
+    require railtie
+  rescue LoadError
+  end
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.

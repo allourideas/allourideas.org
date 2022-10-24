0c15caac3600281699a85b2ef08f3625741b4c50
Robert Bjarnason <robert.bjarnason@gmail.com>
2022-10-24 22:05:34 +0000

    Work on deployment

--- deploy/docker/pairwise-api/setup_app.sh
+++ deploy/docker/pairwise-api/setup_app.sh
@@ -5,8 +5,14 @@ git clone https://github.com/CitizensFoundation/pairwise-api.git
 
 chown -R app:app /home/app/pairwise-api
 cd /home/app/pairwise-api
+ls -l
 
 sudo -u app bundle install
 sudo -u app bundle exec rake db:create RAILS_ENV=production
 sudo -u app bundle exec rake db:schema:load RAILS_ENV=production
 sudo -u app bundle exec rake db:seed RAILS_ENV=production
+
+bundle install
+bundle exec rake db:create RAILS_ENV=production
+bundle exec rake db:schema:load RAILS_ENV=production
+bundle exec rake db:seed RAILS_ENV=production

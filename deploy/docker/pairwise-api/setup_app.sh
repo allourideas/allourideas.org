51999f778faaa57f17cca71bf6960c881ca9b758
Robert Bjarnason <robert.bjarnason@gmail.com>
2022-10-24 21:53:00 +0000

    Work on deployment

--- deploy/docker/pairwise-api/setup_app.sh
+++ deploy/docker/pairwise-api/setup_app.sh
@@ -1,16 +1,8 @@
 #! /bin/bash
-chown -R app:app /root/pairwise-api
-cd /root
-ls -la
-cd /root/pairwise-api
-ls -la
-cd /root/pairwise-api
-pwd
-ls -la
-pwd
-sudo -u app pwd
-sudo -u app bundle install
-sudo -u app bash -c "cd /root/pairwise-api;bundle exec rake db:create RAILS_ENV=production"
-sudo -u app bash -c "cd /root/pairwise-api;bundle exec rake db:schema:load RAILS_ENV=production"
-sudo -u app bash -c "cd /root/pairwise-api;bundle exec rake db:seed RAILS_ENV=production"
+chown -R app:app /home/app/pairwise-api
+cd /home/app/pairwise-api
 
+sudo -u app bundle install
+sudo -u app bundle exec rake db:create RAILS_ENV=production
+sudo -u app bundle exec rake db:schema:load RAILS_ENV=production
+sudo -u app bundle exec rake db:seed RAILS_ENV=production

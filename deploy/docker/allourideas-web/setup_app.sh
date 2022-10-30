89b8de8bc20dac386975fbb214b7dad3aa603134
Robert Bjarnason <robert.bjarnason@gmail.com>
2022-10-30 20:17:12 +0000

    Work on upgrade

--- deploy/docker/allourideas-web/setup_app.sh
+++ deploy/docker/allourideas-web/setup_app.sh
@@ -2,7 +2,7 @@
 mkdir -p /home/app
 cd /home/app
 
-rm -r allourideas.org
+#rm -r allourideas.org
 
 git clone https://github.com/CitizensFoundation/allourideas.org.git
 chown -R app:app /home/app/allourideas.org
@@ -15,3 +15,4 @@ ls -l config
 sudo -E -u app bundle install
 sudo -E -u app bundle exec rake db:create RAILS_ENV=production
 sudo -E -u app bundle exec rake db:schema:load RAILS_ENV=production
+true

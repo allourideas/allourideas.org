e38cba4bf54e68057e98124d269ecce3c08e8671
Robert Bjarnason <robert.bjarnason@gmail.com>
2023-06-11 14:08:37 +0000

    Work on bugsnag

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,3 +1,8 @@
 Bugsnag.configure do |config|
   config.api_key = ENV['AOI_BUGSNAG_API_KEY']
+  config.add_on_error(proc { |report|
+    if report.error_class == 'AbstractController::ActionNotFound'
+      report.ignore!
+    end
+  })
 end
